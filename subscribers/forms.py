760bb71fbfcae8abdb4fd5fe840a9c9f848cc412
Denis Doktorov <denisd581@gmail.com>
2022-11-18 09:13:31 +0300

    Delete unnecessary app

--- subscribers/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-
-from .models import Subscriber
-
-
-class SubscriberForm(forms.ModelForm):
-    class Meta:
-        model = Subscriber
-        fields = ('first_name', 'last_name', 'birthdate', 'email')
