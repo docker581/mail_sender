c4df363b96e7db8f27a955220effdc9a14e558b6
Denis Doktorov <denisd581@gmail.com>
2022-11-21 02:55:03 +0300

    Add layouts-subscribers functional

--- data/urls.py
+++ data/urls.py
@@ -5,8 +5,8 @@ from . import views
 urlpatterns = [ 
     url(
         'layouts-and-subscribers', 
-        views.get_layouts_and_subscribers,
-        name='layouts_subscribers'
+        views.show_layouts_and_subscribers,
+        name='layouts_subscribers',
     ),
     url('add-layout', views.add_layout, name='add_layout'),
     url('add-subscriber', views.add_subscriber, name='add_subscriber'),
--- data/views.py
+++ data/views.py
@@ -1,21 +1,45 @@
 # -*- coding: utf-8 -*-
-from django.shortcuts import render, redirect, HttpResponse
+from django.shortcuts import render, redirect
 
 from .models import Layout, Subscriber
 from .forms import  LayoutForm, SubscriberForm
 
 
-def index(request):
-    return render(request, 'index.html')
-
-
-def get_layouts_and_subscribers(request):
+def get_layouts_and_subscribers():
     layouts = Layout.objects.all()
     subscribers = Subscriber.objects.all()
+    return layouts, subscribers
+
+
+def index(request):
+    layouts, subscribers = get_layouts_and_subscribers()
+    if not layouts or not subscribers:
+        is_sending_ready = False
+    else:
+        is_sending_ready = True    
+    return render(request, 'index.html', {'is_sending_ready': is_sending_ready})
+
+
+def set_message_to_session(request, message):
+    request.session['message'] = message
+
+
+def get_message_from_session(request):
+    if request.session.get('message'):
+        message = request.session.get('message')
+        del request.session['message']
+    else:
+        message = None    
+    return message 
+
+
+def show_layouts_and_subscribers(request):
+    layouts, subscribers = get_layouts_and_subscribers()
+    message = get_message_from_session(request)  
     return render(
         request, 
         'layouts_subscribers.html', 
-        {'layouts': layouts, 'subscribers': subscribers}
+        {'layouts': layouts, 'subscribers': subscribers, 'message': message}
     )
 
 
@@ -24,13 +48,10 @@ def add_layout(request):
     if request.method == 'POST':
         if form.is_valid():
             form.save()
-            message = 'Добавлен новый макет!'
-            return redirect('layouts_subscribers', message)
-    return render(
-        request, 
-        'add_layout.html', 
-        {'form': form, 'form_button_name': 'Добавить'}
-    )
+            message = 'Добавлен новый макет письма!'
+            set_message_to_session(request, message)
+            return redirect('layouts_subscribers')
+    return render(request, 'add_layout.html', {'form': form})
 
 
 def add_subscriber(request):
@@ -39,9 +60,6 @@ def add_subscriber(request):
         if form.is_valid():
             form.save()
             message = 'Добавлен новый подписчик!'
-            return redirect('layouts_subscribers', message)
-    return render(
-        request, 
-        'add_subscriber.html', 
-        {'form': form, 'form_button_name': 'Добавить'}
-    )
+            set_message_to_session(request, message)
+            return redirect('layouts_subscribers')
+    return render(request, 'add_subscriber.html', {'form': form})
